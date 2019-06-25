8f3c3c43d7b581bb0519c2e86a60149e2abaa163
aixin <aixintian@sf-express.com>
2019-06-25 13:14:39 +0800

    Fix: answer code

--- src/main/java/cn/tianaixin/question/q1/TwoSum.java
+++ src/main/java/cn/tianaixin/question/q1/TwoSum.java
@@ -7,30 +7,31 @@ import java.util.HashMap;
  */
 public class TwoSum {
     public static void main(String[] args) {
-        int nums[] = {1,2,3,4};
+        int nums[] = {1,3,3,3,4};
         int target = 6;
 
         TwoSum twoSum = new TwoSum();
         int[] res = twoSum.twoSum(nums, target);
         for (int i = 0; i < res.length; i++) {
-            System.out.print(res[i]);
+            System.out.println(res[i]);
         }
     }
 
     public int[] twoSum(int[] nums, int target) {
-        HashMap<Integer, Boolean> map = new HashMap<Integer, Boolean>();
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 
         for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], true);
+            map.put(nums[i], i);
         }
 
         int[] a = new int[2];
         for (int i = 0; i < nums.length; i++) {
             int x = nums[i];
             int y = target - x;
+
             if (map.containsKey(y)) {
-                a[0] = x;
-                a[1] = y;
+                a[0] = i;
+                a[1] = map.get(y);
                 return a;
             }
         }
